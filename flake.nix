9ec72a38dad2dd598589a6ff7c539eefccf6d72f
Sergey Yakovlev <selfuryon@pm.me>
2023-11-15 22:19:05 +0200

    chore: add nix-github-actions

--- flake.nix
+++ flake.nix
@@ -4,6 +4,10 @@
   inputs = {
     # Nix stuff
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nix-github-actions = {
+      url = "github:nix-community/nix-github-actions";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     # flake-parts
     flake-parts = {
--- nix/default.nix
+++ nix/default.nix
@@ -1,4 +1,9 @@
-{inputs, ...}: {
+{
+  inputs,
+  lib,
+  self,
+  ...
+}: {
   imports = [
     inputs.devshell.flakeModule
     inputs.pre-commit-hooks-nix.flakeModule
@@ -6,4 +11,9 @@
     ./pre-commit.nix
     ./shell.nix
   ];
+  flake = {
+    githubActions = inputs.nix-github-actions.lib.mkGithubMatrix {
+      checks = lib.getAttrs ["x86_64-linux"] self.checks;
+    };
+  };
 }
