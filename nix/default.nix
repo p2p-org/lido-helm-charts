9ce1d794cd662a2f38e0a43964c9607412e19015
Sergey Yakovlev <selfuryon@pm.me>
2023-11-16 10:32:05 +0000

    chore: improvements (#5)
    * feat(workflows): add integration workflow
    
    * chore: rename tmpl -> tpl
    
    * feat: add nix to integration workflow
    
    * fix: fix links in README

--- nix/default.nix
+++ nix/default.nix
@@ -1,12 +1,19 @@
-{inputs, ...}: {
+{
+  inputs,
+  lib,
+  self,
+  ...
+}: {
   imports = [
-    inputs.flake-parts.flakeModules.easyOverlay
     inputs.devshell.flakeModule
     inputs.pre-commit-hooks-nix.flakeModule
-    ./env.nix
     ./formatter.nix
     ./pre-commit.nix
     ./shell.nix
-    ./tools.nix
   ];
+  flake = {
+    githubActions = inputs.nix-github-actions.lib.mkGithubMatrix {
+      checks = lib.getAttrs ["x86_64-linux"] self.checks;
+    };
+  };
 }
--- nix/shell.nix
+++ nix/shell.nix
@@ -1,32 +1,12 @@
 {
   perSystem = {
     pkgs,
-    inputs',
     config,
     ...
-  }: let
-    inherit (inputs'.ethereum-nix.packages) ethdo;
-    inherit (inputs'.colmena.packages) colmena;
-    google-env = pkgs.google-cloud-sdk.withExtraComponents [pkgs.google-cloud-sdk.components.gke-gcloud-auth-plugin];
-  in {
+  }: {
     devshells.default = {
-      name = "etherno";
-      packages = with pkgs; [
-        terraform
-        terragrunt
-        awscli2
-        kubectl
-        kubernetes-helm
-        krew
-        kubectx
-        k9s
-        yaml-language-server
-        shellcheck
-        statix
-        ethdo
-        colmena
-        google-env
-      ];
+      name = "lido";
+      packages = with pkgs; [kubernetes-helm];
       devshell.startup = {
         pre-commit.text = config.pre-commit.installationScript;
       };
